bad0899de66e5663de443a11a4ec2fd18c8594b3
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-10-21 19:02:01 +0300

    ch13-02

---
+++ iters/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    // iter1();
+    iter2();
+}
+
+fn iter2() {
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iter1() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+
+        // for val in v1_iter {
+        //     println!("Got: {}", val);
+        //
+        // }
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+}
