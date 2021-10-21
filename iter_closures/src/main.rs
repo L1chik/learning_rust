3e661ea330939bd73f013f652321b2afacb29672
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-10-21 14:10:48 +0300

    ch13-01 done

--- iter_closures/src/main.rs
+++ iter_closures/src/main.rs
@@ -1,11 +1,17 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
 fn main() {
     let simulated_user_specidied_value = 10;
     let simulated_random_number = 7;
 
-    generated_workout4(simulated_user_specidied_value, simulated_random_number);
+    // generated_workout4(simulated_user_specidied_value, simulated_random_number);
+    // generated_workout4(5, 7);
+
+    catch_variables();
+    // catch_variables2();
+    catch_variables3();
 
     // test();
 }
@@ -94,12 +100,20 @@ fn test() {
     // let n = example_closure(5);
 }
 
+// struct Cacher<T>
+//     where
+//         T: Fn(u32) -> u32,
+// {
+//     calculation: T,
+//     value: Option<u32>,
+// }
+
 struct Cacher<T>
     where
         T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: HashMap<Option<u32>>,
+    value: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -107,21 +121,32 @@ impl<T> Cacher<T>
 {
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
-            calculation: HashMap,
-            value: None,
+            calculation,
+            value: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
+        // let calc = &self.calculation;
+        // self.value.entry(arg).or_insert_with(|| calc(arg))
 
-                v
-            }
+        if self.value.contains_key(&arg) {
+            self.value[&arg]
+        } else {
+            self.value.insert(arg, (self.calculation)(arg));
+
+            self.value[&arg]
         }
+
+        // match self.value {
+        //     Some(v) => v,
+        //     None => {
+        //         let v = (self.calculation)(arg);
+        //         self.value = Some(v);
+        //
+        //         v
+        //     }
+        // }
     }
 }
 
@@ -148,12 +173,46 @@ fn generated_workout4(intensity: u32, random_number: u32) {
     }
 }
 
- #[test]
+#[test]
 fn call_with_different_values() {
-     let mut c = Cacher::new(|a| a);
+    let mut c = Cacher::new(|a| a);
 
-     let v1 = c.value(1);
-     let v2 = c.value(2);
+    let v1 = c.value(1);
+    let v2 = c.value(2);
 
-     assert_eq!(v2, 2);
- }
+    assert_eq!(v2, 2);
+}
+
+fn catch_variables() {
+    let x = 4;
+
+    let equal_to_x = |z| z == x; // Имеет типаж Fn, т.к. тело замыкание должно только читать знач. в x
+
+    let y = 4;
+
+    assert!(equal_to_x(y));
+}
+
+// fn catch_variables2 () {
+//     let x = 4;
+//
+//     fn equl_to_x(z: i32) -> bool {
+//         z == x
+//     }
+//
+//     let y = 4;
+//
+//     assert!(equl_to_x(y));
+// }
+
+fn catch_variables3 () {
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
+    assert!(equal_to_x(y));
+}
