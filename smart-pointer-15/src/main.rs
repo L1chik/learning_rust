4f0d025c783c19e186401cc0c8b174128009fc35
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-11-06 01:00:48 +0300

    added 15.2, 15.3, 15.4

--- smart-pointer-15/src/main.rs
+++ smart-pointer-15/src/main.rs
@@ -1,14 +1,117 @@
 use crate::List::{Cons, Nil};
-
+use std::rc::Rc;
+use std::ops::Deref;
+// use crate::List2::Cons as OtherCons;
 fn main() {
     // let b = Box::new(5);
     // println!("b = {}", b);
 
     // let list = Cons(1, Cons(2, Cons(3, Nil)));
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let list = Cons(1, Box::new(Cons(
+    //     2, Box::new(Cons(3, Box::new(Nil))))));
+
+    deref_op();
+    deref_op2();
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    smart_pointer_drop();
+
+    rc_pointer();
+}
+
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+fn deref_op() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+}
+
+fn deref_op2() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn deref_op3() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data '{}'!", self.data);
+    }
+}
+
+fn smart_pointer_drop() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    // let d = CustomSmartPointer {
+    //     data: String::from("other stuff"),
+    // };
+    println!("CustomSmartPointers created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
+}
+
+fn rc_pointer() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
+
+fn inter_mut() {
+    let x = 5;
+    let y = &mut x;
 }
