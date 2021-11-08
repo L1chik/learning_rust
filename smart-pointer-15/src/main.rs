f7942750308c4c3ba66425f272318e2d54c6be95
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-11-08 23:28:07 +0300

    15.5 done

--- smart-pointer-15/src/main.rs
+++ smart-pointer-15/src/main.rs
@@ -18,7 +18,9 @@ fn main() {
 
     smart_pointer_drop();
 
-    rc_pointer();
+    // rc_pointer();
+
+    new_test();
 }
 
 // enum List {
@@ -94,24 +96,45 @@ fn smart_pointer_drop() {
     println!("CustomSmartPointer dropped before the end of main.");
 }
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+//
+// fn rc_pointer() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
-fn rc_pointer() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// use crate::List::{Cons, Nil};
+use std::cell::{Ref, RefCell};
+// use std::rc::Rc;
+
+fn new_test() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
-fn inter_mut() {
-    let x = 5;
-    let y = &mut x;
-}
