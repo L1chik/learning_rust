6efa274df7a335486d329696cabe35f320abdc9b
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-11-14 00:19:38 +0300

    16 done

--- r16-threads/src/main.rs
+++ r16-threads/src/main.rs
@@ -1,13 +1,19 @@
+use std::rc::Rc;
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
+
 
 fn main() {
     // threads1();
     // threads2();
     // threads3();
     // threads4();
-    threads5();
+    // threads5();
+
+    // mutex();
+    mutex2();
 }
 
 fn threads1() {
@@ -110,4 +116,36 @@ fn threads5 () {
     for received in rx {
         println!("Got: {}", received);
     }
+}
+
+fn mutex() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+fn mutex2() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
